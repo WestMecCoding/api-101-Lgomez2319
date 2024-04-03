520ee7c61dd1883e0f7a64b98f8b989f911bc088
Lily <142362877+Lgomez2319@users.noreply.github.com>
2024-04-03 00:27:53 +0000

    xhr stuff

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// const cors = require('cors');
 const app = express();
+// app.use(cors);
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+    // res.send('Hello World');
+});
+
+app.get('/csv-data', (req, res) => {
+    const csvPath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv');
+    fs.readFile(csvPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return
+        }
+        res.type('text/plain');
+        res.send(data);
+    });
+
 });
 
 app.get('/name', (req, res) => {
@@ -13,3 +31,4 @@ app.get('/name', (req, res) => {
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
 });
+
